943c4ee384b18f11d836e937f46f8610986f0e7c
HunterX18 <srsahu13@gmail.com>
2024-01-20 16:49:32 +0530

    set up basic apis

---
+++ server/products.js
@@ -0,0 +1,19 @@
+const products = {
+	phone: {
+		price: 100,
+	},
+	laptop: {
+		price: 200,
+	},
+	tv: {
+		price: 500,
+	},
+	shirt: {
+		price: 100,
+	},
+	pants: {
+		price: 300,
+	},
+};
+
+export default products;
